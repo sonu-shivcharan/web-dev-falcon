efd0b1d048c00aca8ea8c60e024032dfad3d7ebe
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-18 08:40:34 +0530

    call connectDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { configDotenv } from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/connectDB.js";
 configDotenv();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongo db connection failed", error);
+  });
