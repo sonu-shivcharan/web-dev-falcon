f735f0d8e6122845b838fce46d91306568d474a6
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-22 13:19:01 +0530

    config cookie parser

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // basic configurations
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
+app.use(cookieParser());
 //cors configuration
 app.use(
   cors({
@@ -24,7 +25,7 @@ app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   const data = req.body;
-  console.log("data", data);
+  // console.log("data", data);
   res.send("Welcome to baseCampy");
 });
 
