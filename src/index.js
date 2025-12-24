5e5666a96255869d16a3a4083483c461cf56ff6c
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-24 11:20:50 +0530

    chnage server startup message

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 connectDB()
   .then(() => {
     app.listen(port, () => {
-      console.log(`Example app listening on port http://localhost:${port}`);
+      console.log(`App listening on port http://localhost:${port}`);
     });
   })
   .catch((error) => {
