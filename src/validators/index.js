b13700ec2a61f89f79024e1fa5ced8b89996ebd2
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-24 11:20:27 +0530

    add validators for chnage passowrd, forget password and reset password route

--- src/validators/index.js
+++ src/validators/index.js
@@ -32,4 +32,32 @@ const userLoginValidator = () => {
   ];
 };
 
-export { userRegisterValidator, userLoginValidator };
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+const userResetPasswordValidator = () => {
+  return [
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetPasswordValidator,
+};
